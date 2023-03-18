fdec50ae171317f33f0b10cbd3d756e89b0486c3
scottishsummer98 <samiremail98@gmail.com>
2023-03-18 09:20:30 +0600

    'contactsfrontresponsive'

--- resources/js/pages/FrontEnd/Contacts.vue
+++ resources/js/pages/FrontEnd/Contacts.vue
@@ -1,146 +1,123 @@
 <template>
-  <section class="section" style="margin-top: 15px;">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-          <h2 class="mb-3">
-            Contact Us
-          </h2>
+  <div class="contact_front">
+    <h3>
+      Contact Us
+    </h3>
+    <div class="row1_contact_front">
+      <div class="col1_row1_contact_front">
+        <img
+          class="image_col1_row1_contact_front"
+          src="../../../../public/images/Dr-Nasir_uddin(2).jpg"
+          alt=""
+        />
+        <p style="text-align: justify; font-size: large; font-family: Tahoma;">
+          PROF. DR. NASIR UDDIN IS ONE OF THE BEST DENTAL SURGEON IN DHAKA,
+          BANGLADESH. HE IS HEAD OF THE DEPARTMENT OF ORAL AND MAXILLOFACIAL
+          SURGERY, SHAHEED SUHRAWARDY MEDICAL COLLEGE AND HOSPITAL AND ONE OF
+          THE LEADING ORAL AND MAXILLOFACIAL SURGEONS.
+        </p>
+      </div>
+      <div class="col2_row1_contact_front">
+        <div class="widget">
           <div
-            style="
-              border-bottom: 3px solid black;
-              border-top: 3px solid black;
-              display: flex;
-            "
+            class="blog-list-widget"
+            style="height: 28rem; background-color: #eeeeee;"
           >
-            <img
-              class="img-fluid"
-              style="margin: 1rem;"
-              src="../../../../public/images/Dr-Nasir_uddin(2).jpg"
-              alt=""
-            />
-            <p
-              style="
-                text-align: justify;
-                margin: 1rem;
-                font-size: large;
-                font-family: Tahoma;
-              "
-            >
-              PROF. DR. NASIR UDDIN IS ONE OF THE BEST DENTAL SURGEON IN DHAKA,
-              BANGLADESH. HE IS HEAD OF THE DEPARTMENT OF ORAL AND MAXILLOFACIAL
-              SURGERY, SHAHEED SUHRAWARDY MEDICAL COLLEGE AND HOSPITAL AND ONE
-              OF THE LEADING ORAL AND MAXILLOFACIAL SURGEONS.
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-          <div class="sidebar">
-            <div class="widget">
-              <div
-                class="blog-list-widget"
-                style="height: 28rem; background-color: #eeeeee;"
-              >
-                <form action="" style="padding: 2rem;">
-                  <h5 class="mb-2">FEEDBACK FORM</h5>
-                  <h6>
-                    Name
-                    <span class="text-danger">*</span>
-                  </h6>
-                  <input
-                    type="text"
-                    style="
-                      border-bottom: 2px solid black;
-                      color: grey;
-                      width: 100%;
-                      padding: 0.5rem;
-                    "
-                  />
-                  <h6 class="mt-2">
-                    Phone
-                    <span class="text-danger">*</span>
-                  </h6>
-                  <input
-                    type="text"
-                    style="
-                      border-bottom: 2px solid black;
-                      color: grey;
-                      width: 100%;
-                      padding: 0.5rem;
-                    "
-                  />
-                  <h6 class="mt-2">
-                    Address
-                    <span class="text-danger">*</span>
-                  </h6>
-                  <input
-                    type="text"
-                    style="
-                      border-bottom: 2px solid black;
-                      color: grey;
-                      width: 100%;
-                      padding: 0.5rem;
-                    "
-                  />
-                  <h6 class="mt-2">
-                    Message
-                    <span class="text-danger">*</span>
-                  </h6>
-                  <input
-                    type="text"
-                    style="
-                      border-bottom: 2px solid black;
-                      color: grey;
-                      width: 100%;
-                      padding: 0.5rem;
-                    "
-                  />
-                  <button class="btn btn-primary mt-4" style="width: 100%;">
-                    SEND MESSAGE
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="row mt-5 mb-5">
-          <div
-            class="col-md-4"
-            v-for="(item, index) in contactsList"
-            :key="index"
-          >
-            <div
-              style="height: 7rem; background-color: gainsboro; padding: 1rem;"
-            >
-              <h5 style="text-align: justify;">
-                {{ item.name }}
-              </h5>
-            </div>
-
-            <div style="border: 1px solid black; padding: 1rem; height: 22rem;">
+            <form action="" style="padding: 2rem;">
+              <h5 class="mb-2">FEEDBACK FORM</h5>
               <h6>
-                <h5><i class="fa-solid fa-building"></i></h5>
-                {{ item.location }}
+                Name
+                <span class="text-danger">*</span>
               </h6>
-              <h6>
-                <h5><i class="fa-solid fa-phone"></i></h5>
-                {{ item.mobile }}
+              <input
+                type="text"
+                style="
+                  border-bottom: 2px solid black;
+                  color: grey;
+                  width: 100%;
+                  padding: 0.5rem;
+                "
+              />
+              <h6 class="mt-2">
+                Phone
+                <span class="text-danger">*</span>
               </h6>
-              <h6>
-                <h5><i class="fa-solid fa-envelope"></i></h5>
-                {{ item.email }}
+              <input
+                type="text"
+                style="
+                  border-bottom: 2px solid black;
+                  color: grey;
+                  width: 100%;
+                  padding: 0.5rem;
+                "
+              />
+              <h6 class="mt-2">
+                Address
+                <span class="text-danger">*</span>
               </h6>
-              <h6>
-                <h5><i class="fa-solid fa-clock"></i></h5>
-                {{ item.visitingtime }}
+              <input
+                type="text"
+                style="
+                  border-bottom: 2px solid black;
+                  color: grey;
+                  width: 100%;
+                  padding: 0.5rem;
+                "
+              />
+              <h6 class="mt-2">
+                Message
+                <span class="text-danger">*</span>
               </h6>
-            </div>
+              <input
+                type="text"
+                style="
+                  border-bottom: 2px solid black;
+                  color: grey;
+                  width: 100%;
+                  padding: 0.5rem;
+                "
+              />
+              <button class="btn btn-primary mt-4" style="width: 100%;">
+                SEND MESSAGE
+              </button>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  </section>
+    <div class="row2_contact_front">
+      <div
+        class="items_row2_contact_front"
+        v-for="(item, index) in contactsList"
+        :key="index"
+      >
+        <div class="itemheader_items_row2_contact_front">
+          <h5 style="text-align: justify;">
+            {{ item.name }}
+          </h5>
+        </div>
+
+        <div class="itembody_items_row2_contact_front">
+          <h6>
+            <h5><i class="fa-solid fa-building"></i></h5>
+            {{ item.location }}
+          </h6>
+          <h6>
+            <h5><i class="fa-solid fa-phone"></i></h5>
+            {{ item.mobile }}
+          </h6>
+          <h6>
+            <h5><i class="fa-solid fa-envelope"></i></h5>
+            {{ item.email }}
+          </h6>
+          <h6>
+            <h5><i class="fa-solid fa-clock"></i></h5>
+            {{ item.visitingtime }}
+          </h6>
+        </div>
+      </div>
+    </div>
+  </div>
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7303.627684756733!2d90.36675132503694!3d23.75401693835033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe0b8d0db2e09450a!2sAvenue%20Dental%20Care!5e0!3m2!1sen!2sbd!4v1619258721916!5m2!1sen!2sbd"
     height="400"
@@ -174,4 +151,104 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.contact_front {
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  padding: 2rem 25rem;
+}
+.row1_contact_front {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+}
+.col1_row1_contact_front {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  border-bottom: 3px solid black;
+  border-top: 3px solid black;
+  width: 70%;
+}
+.image_col1_row1_contact_front {
+  width: 25rem;
+  height: 25rem;
+}
+.col2_row1_contact_front {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  width: 30%;
+}
+.row2_contact_front {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+}
+.items_row2_contact_front {
+  width: 33%;
+}
+.itemheader_items_row2_contact_front {
+  height: 7rem;
+  background-color: gainsboro;
+  padding: 1rem;
+}
+.itembody_items_row2_contact_front {
+  border: 1px solid black;
+  padding: 1rem;
+  height: 22rem;
+}
+/* Media Queries Medium Devices*/
+@media screen and (max-width: 1024px) {
+  .contact_front {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    padding: 2rem;
+  }
+  .row1_contact_front {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+  .col1_row1_contact_front {
+    width: 100%;
+  }
+  .col2_row1_contact_front {
+    width: 100%;
+  }
+  .itemheader_items_row2_contact_front {
+    height: 12rem;
+  }
+  .itembody_items_row2_contact_front {
+    height: 24rem;
+  }
+}
+
+/* Media Queries Small Devices*/
+@media screen and (max-width: 600px) {
+  .col1_row1_contact_front {
+    flex-direction: column;
+    width: 100%;
+  }
+  .image_col1_row1_contact_front {
+    width: 20rem;
+    height: 20rem;
+  }
+  .row2_contact_front {
+    flex-direction: column;
+    gap: 1rem;
+  }
+  .items_row2_contact_front {
+    width: 100%;
+  }
+  .itemheader_items_row2_contact_front {
+    height: 7.5rem;
+  }
+  .itembody_items_row2_contact_front {
+    height: 19rem;
+  }
+}
+</style>
